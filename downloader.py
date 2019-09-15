268e0535f3fa69626ff9c50f40a71f72fc9ce381
Kaminda <kaminda@sprii.com>
2019-09-15 21:39:31 +0400

    Updated on 15/09/2019

--- downloader.py
+++ downloader.py
@@ -2,13 +2,23 @@ from __future__ import unicode_literals
 import youtube_dl
 import csv
 
+# options for youtube_dl f means the quality here we specified as the best available
+# Other available values for this parameter
+#    best: Select the best quality format represented by a single file with video and audio.
+#    worst: Select the worst quality format represented by a single file with video and audio.
+#    bestvideo: Select the best quality video-only format (e.g. DASH video). May not be available.
+#    worstvideo: Select the worst quality video-only format. May not be available.
+#    bestaudio: Select the best quality audio only-format. May not be available.
+#    worstaudio: Select the worst quality audio only-format. May not be available.
+# Refer here for other options and values https://github.com/ytdl-org/youtube-dl/blob/master/README.md
+
 ydl_opts = {
             'f': 'best'
         }
 
-with open('url_list.csv') as csv_file:
+with open('url_list.csv') as csv_file: #Opening the CSV file with url list
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    for row in csv_reader:
+    for row in csv_reader: # Loopjing through the url's
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([row[0]])
+            ydl.download([row[0]]) #asking youtube_dl to dowload the current url from csv list
---
+++ read_me.txt
@@ -0,0 +1,15 @@
+This small script will allow you to download list of youtube videos using youtube_dl python library.
+
+Usage
+1. First install the youtube_dl library
+    pip install youtube_dl
+
+2. Then you can need to insert the list of youtube urls in to url_list.csv file. Each url needs to be in a new line
+
+3. Run the script to start the download
+    python downloader.py
+
+4. Videos in the url list will be downloaded in to the root where this script resides in your local machine.
+
+
+Reach out if you have question at kaminda.berugoda@gmail.com
